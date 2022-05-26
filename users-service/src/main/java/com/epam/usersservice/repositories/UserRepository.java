22f8c4014d1f6e434eb7be24d1629f8c62c674eb
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2022-05-26 09:56:32 +0530

    Delete users-service directory

--- users-service/src/main/java/com/epam/usersservice/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epam.usersservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.epam.usersservice.entities.User;
-
-public interface UserRepository extends JpaRepository<User,Integer>{
-	
-}
